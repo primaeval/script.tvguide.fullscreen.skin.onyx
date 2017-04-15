6a2ff2a244cf0ba90fbf05f25d365a574bced873
primaeval <primaeval@users.noreply.github.com>
2017-04-15 12:31:28 +0200

    Onyx with Categories

--- addon.py
+++ addon.py
@@ -1,13 +1,18 @@
 import xbmc,xbmcgui,xbmcaddon
 
+SKINS = [["Onyx", "Onyx"],
+         ["Onyx with Categories",  "OnyxC"]]
+
+
 d = xbmcgui.Dialog()
-ok = d.ok("TV Guide Fullscreen","Set this Skin as default?")
-if ok:
+names = [s[0] for s in SKINS]
+skin = d.select("TV Guide Fullscreen - Set Default Skin", names)
+if skin > -1:
     tvgf = xbmcaddon.Addon("script.tvguide.fullscreen")
     if tvgf:
         tvgf.setSetting('skin.source', '2')
         tvgf.setSetting('skin.folder', 'special://home/addons/script.tvguide.fullscreen.skin.onyx/')
-        tvgf.setSetting('skin.user', 'Onyx')
+        tvgf.setSetting('skin.user', SKINS[skin][1])
         tvgf.setSetting('epg.nofocus.color', '[COLOR ffd3d3d3]lightgray[/COLOR]')
         tvgf.setSetting('epg.focus.color', '[COLOR ffffffff]white[/COLOR]')
         tvgf.setSetting('timebar.color', '[COLOR ffb22222]firebrick[/COLOR]')
